0b5d4cdc1b9dfcb573d16e43312823580e7347f9
ricardo_msi_studio <ricardo@azevem.com>
2018-08-15 17:56:31 -0300

    damn, we didn't need the ./ thing

--- files/justforfun/justforfun/local_settings.py
+++ files/justforfun/justforfun/local_settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&auwlm+_glkz(zzx_sy$lotl^04pe2v3&a3zx_m3!ov%75#pka'
+SECRET_KEY = '&auwlm+_glkFEHAOSDIFJkhsdfoihjio#$@4gfasd3zx_m3!ov%75#pka'
 
 
 # Database
@@ -12,8 +12,12 @@ SECRET_KEY = '&auwlm+_glkz(zzx_sy$lotl^04pe2v3&a3zx_m3!ov%75#pka'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'justforfun',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
